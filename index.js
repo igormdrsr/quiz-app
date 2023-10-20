5779dc55222be746b2814040393091d1ef2e2762
Igor Rocha <igor7.mdrs@gmail.com>
2023-10-20 09:13:06 -0300

    save progress

--- index.js
+++ index.js
@@ -36,7 +36,9 @@ const quizData = [
 const submitButton = document.querySelector(".submit");
 const quiz = document.getElementById("quiz");
 const answerEls = document.querySelectorAll(".answer");
-const questionEl = document.querySelector(".question-title");
+const questionTitle = document.querySelector(".question-title");
+
+// answers's label
 const a_text = document.getElementById("a_text");
 const b_text = document.getElementById("b_text");
 const c_text = document.getElementById("c_text");
@@ -48,17 +50,43 @@ let score = 0;
 loadQuiz();
 
 submitButton.addEventListener("click", () => {
-  console.log("teste");
+  const answer = getAnswer();
+  if (answer === quizData[currentQuestion].correct) {
+    score++;
+  }
   currentQuestion++;
-  loadQuiz();
+  if (currentQuestion < quizData.length) {
+    loadQuiz();
+  } else {
+    alert(`Parabéns! Sua pontuação foi ${score}/${quizData.length}`);
+  }
 });
 
 function loadQuiz() {
+  deselectAnswers();
   const currentQuizData = quizData[currentQuestion];
 
-  questionEl.innerText = currentQuizData.question;
+  questionTitle.innerText = currentQuizData.question;
   a_text.innerText = currentQuizData.a;
   b_text.innerText = currentQuizData.b;
   c_text.innerText = currentQuizData.c;
   d_text.innerText = currentQuizData.d;
+}
+
+function deselectAnswers() {
+  answerEls.forEach((answerEl) => {
+    answerEl.checked = false;
+  });
+}
+
+function getAnswer() {
+  let answer;
+
+  answerEls.forEach((element) => {
+    if (element.checked) {
+      answer = element.value;
+    }
+  });
+
+  return answer;
 }
